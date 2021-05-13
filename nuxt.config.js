27a4077e4a90b5e95e29d5d725f3d4a55e3098d4
Alexey Protopopov <alekseiii96@gmail.com>
2021-05-13 23:16:16 +0300

    Save uncommited changes (WIP)

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  target: 'static',
   head: {
     titleTemplate: '%s - nuxt-base-authentication',
     title: 'nuxt-base-authentication',
